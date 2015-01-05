2671b83045f6d679e206ba786cb44795c7c730aa
Annabel Jump <annabel.jump@gmail.com>
2015-01-05 20:51:41 +0000

    Added Impl file with placeholder methods

---
+++ ReturnObjectImpl.java
@@ -0,0 +1,15 @@
+/**
+ *  A class implementing ReturnObject interface.
+ *  By Annabel Jump for Programming in Java Coursework 3
+ */
+
+public class ReturnObjectImpl implements ReturnObject {
+
+    public boolean hasError();
+
+    public ErrorMessage getError();
+
+    public Object getReturnValue();
+    
+
+}
