57736b75a8adeef380c7835097367fbeea08b059
Annabel Jump <annabel.jump@gmail.com>
2015-02-01 14:52:01 +0000

    Added removal of object at position in array

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -36,18 +36,43 @@ public class ArrayList implements List {
         return size;
     }
     /**
-     * Returns the elements at the given position.
-     * Determines if requested position does not exist
+     * Returns the elements at the given position
      * @param index the position in the list of the item to be retrieved
      * @return the element or an appropriate error message,
      *         encapsulated in a ReturnObject
      */
     @Override
     public ReturnObject get(int index) {
+        //determines whether requested position exists
         if(index < 0 || index >= size){
             return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
         } else {
             return new ReturnObjectImpl(myArray[index]);
+        }
+    }
+
+    /**
+     * Returns the elements at the given position and removes it
+     * from the list. The indeces of elements after the removed
+     * element must be updated accordignly.
+     *
+     * @param index the position in the list of the item to be retrieved
+     * @return the element or an appropriate error message,
+     *         encapsulated in a ReturnObject
+     */
+    @Override
+    public ReturnObject remove(int index){
+        //use get method to supply the element to return (or return error for invalid index)
+        ReturnObject removedObject = get(index);
+        //object replaced and indices updated accordingly - if no error returned
+        if(!removedObject.hasError()) {
+            for (i = index; i < size; i++) {
+                myArray[i] = myArray[i + 1];
+            }
+            size--;
+            return removedObject;
+        } else {
+            return removedObject;
         }
     }
 }
