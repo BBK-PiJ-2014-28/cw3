d2e8c3fdb80bed9edaa5c1ab00d9dcd4bd7a6946
Annabel Jump <annabel.jump@gmail.com>
2015-02-01 15:22:09 +0000

    Added add(object) method

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -66,13 +66,44 @@ public class ArrayList implements List {
         ReturnObject removedObject = get(index);
         //object replaced and indices updated accordingly - if no error returned
         if(!removedObject.hasError()) {
-            for (i = index; i < size; i++) {
+            for (int i = index; i < size; i++) {
                 myArray[i] = myArray[i + 1];
             }
             size--;
             return removedObject;
         } else {
             return removedObject;
+        }
+    }
+
+    /**
+     * Adds an element to the list, inserting it at the given
+     * position. The indeces of elements at and after that position
+     * must be updated accordignly.
+     *
+     * @param index the position at which the item should be inserted in
+     *              the list
+     * @param item the value to insert into the list
+     * @return an ReturnObject, empty if the operation is successful
+     *         the item added or containing an appropriate error message
+     */
+    @Override
+    public ReturnObject add(int index, Object item){
+        //no null objects allowed
+        if(item == null){
+            return new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
+        //index error if negative or >= size of list
+        } else if(index < 0 || index >= size) {
+            return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
+        } else {
+            //shift values up the array
+            size++;
+            for(int i = size - 1; i > index; i--) {
+                myArray[i] = myArray[i - 1];
+            }
+            myArray[index] = item;
+            //no error, null/empty returned
+            return null;
         }
     }
 }
