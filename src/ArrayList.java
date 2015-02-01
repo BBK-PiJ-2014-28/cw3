1c58a9452bf0c371fe26392379e122791ee7b78e
Annabel Jump <annabel.jump@gmail.com>
2015-02-01 13:08:20 +0000

    Implemented more methods in ArrayList

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -22,5 +22,35 @@ public class ArrayList implements List {
      * @return true if the list is empty, false otherwise.
      */
     @Override
-    public boolean isEmpty();
+    public boolean isEmpty() {
+        if(size == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Returns the number of items currently in the list.
+     *
+     * @return the number of items currently in the list
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+    /**
+     * Returns the elements at the given position.
+     *
+     * If the index is negative or greater or equal than the size of
+     * the list, then an appropriate error must be returned.
+     *
+     * @param index the position in the list of the item to be retrieved
+     * @return the element or an appropriate error message,
+     *         encapsulated in a ReturnObject
+     */
+    @Override
+    public ReturnObject get(int index) {
+    //tbc
+    }
 }
