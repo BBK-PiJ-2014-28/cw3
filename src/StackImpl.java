9b5ca471aebe6ccb1b381ef1c7bd269425a0e90d
Annabel Jump <annabel.jump@gmail.com>
2015-02-02 18:30:59 +0000

    First Method added

--- src/StackImpl.java
+++ src/StackImpl.java
@@ -6,6 +6,24 @@
 
 public class StackImpl extends AbstractStack {
 
+    //Create list
+    public StackImpl(List list){
+        super(list);
+    }
 
+    /**
+     * Methods from Stack needed to extend AbstractStack specifically
+     * because AbstractStack uses a PROTECTED list. Must link the
+     * protected list to the public list in AbstractStack
+     * {@InheritDoc}
+     */
+    @Override
+    public boolean isEmpty(){
+        if(internalList.isEmpty()){
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
