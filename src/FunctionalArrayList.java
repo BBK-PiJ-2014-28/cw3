842c698aa0826f768ee9e7ce3b29f6ce5ff8f686
Annabel Jump <annabel.jump@gmail.com>
2015-02-02 14:02:19 +0000

    Added FunctionalArrayList (empty)

---
+++ src/FunctionalArrayList.java
@@ -0,0 +1,12 @@
+/**
+ *  A class implementing FunctionalList interface
+ *  Extends ArrayList
+ *  By Annabel Jump for Programming in Java Coursework 3
+ *  @author Annabel Jump
+ */
+
+public class FunctionalArrayList extends ArrayList implements FunctionalList {
+
+
+    
+}
