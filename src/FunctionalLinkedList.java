d225c3784fc8034640dede57c98eeca8990047a3
Annabel Jump <annabel.jump@gmail.com>
2015-02-02 15:38:08 +0000

    Added rest() method - I think

--- src/FunctionalLinkedList.java
+++ src/FunctionalLinkedList.java
@@ -19,5 +19,29 @@ public class FunctionalLinkedList extends LinkedList implements FunctionalList{
         }
     }
 
+    /**
+     * {@InheritDoc}
+     */
+    @Override
+    public FunctionalList rest(){
+        FunctionalList requestedList = new FunctionalLinkedList();
+        if(isEmpty()){
+            return requestedList;
+        } else {
+            /**
+             * For the life of me I cannot see how this should be different
+             * to the FunctionalArrayList - all differences in data structure
+             * are dealt with in the LinkedList methods (which are far more complicated)
+             * So, once again here is a For loop to transfer the list values to the
+             * new list, starting at i=1 to cut out the head of the list
+             */
+            for(i = 1; i < size(); i++){
+                Object listObject = get(i).getReturnValue();
+                requestedList.add(listObject);
+            }
+            return requestedList;
+        }
+    }
+
 
 }
