e5de3019d47e96c3cb58d026f4cc850f75fb4898
Annabel Jump <annabel.jump@gmail.com>
2015-02-02 15:56:55 +0000

    Tidied up with @InheritDoc

--- src/SampleableListImpl.java
+++ src/SampleableListImpl.java
@@ -6,10 +6,7 @@
 
 public class SampleableListImpl extends ArrayList implements SampleableList{
     /**
-     * Returns a list containing the first, third, fifth...
-     * items of this list, or an empty list if the list is empty.
-     *
-     * @return a list containing the first, third, fifth... items of this list
+     * {@InheritDoc}
      */
     public SampleableList sample(){
         SampleableList requestedSample = new SampleableListImpl();
