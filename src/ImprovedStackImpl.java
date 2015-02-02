7d4e0d852d465d54bbf0350ad631cb1857404d3f
Annabel Jump <annabel.jump@gmail.com>
2015-02-02 18:59:11 +0000

    First commit ImprovedStackImpl (empty)

---
+++ src/ImprovedStackImpl.java
@@ -0,0 +1,10 @@
+/**
+ *  An implementation of ImprovedStack
+ *  By Annabel Jump for Programming in Java Coursework 3
+ *  @author Annabel Jump
+ *  {@InheritDoc}
+ */
+
+public class ImprovedStackImpl implements ImprovedStack {
+    
+}
