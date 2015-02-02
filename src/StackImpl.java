a68f0e4fb69297b1117eae2b3c969260c3c6c183
Annabel Jump <annabel.jump@gmail.com>
2015-02-02 18:52:52 +0000

    Last StackImpl methods added

--- src/StackImpl.java
+++ src/StackImpl.java
@@ -38,8 +38,41 @@ public class StackImpl extends AbstractStack {
     /**
      *{@InheritDoc}
      */
+    @Override
     public void push(Object item){
         internalList.add(item);
+        //adding with just Object parameter adds to end/top of list
     }
-    
+
+    /**
+     * {@InheritDoc}
+     */
+    @Override
+    public ReturnObject top(){
+        ReturnObject requestedObject;
+        int size = internalList.size();
+        //size - 1 should be last/top variable in list
+        requestedObject = internalList.get(size - 1);
+        /**
+         * internal measures within the get() methods should
+         * take care of returning errors for empty stacks
+         * get() methods do not modify the list itself
+         */
+        return requestedObject;
+    }
+
+    /**
+     * {@InheritDoc}
+     */
+    @Override
+    public ReturnObject pop(){
+        ReturnObject requestedObject;
+        int size = internalList.size();
+        /**
+         * remove() methods in list implementations should remove from the
+         * list; setting size - 1 means last/top variable should be removed
+         */
+        requestedObject = internalList.remove(size - 1);
+        return requestedObject;
+    }
 }
