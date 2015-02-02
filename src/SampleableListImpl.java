c6f3acb0a44bf9bd33e805bdb411aaa794219bcc
Annabel Jump <annabel.jump@gmail.com>
2015-02-02 15:47:28 +0000

    Decided to extend ArrayList in Implementation

--- src/SampleableListImpl.java
+++ src/SampleableListImpl.java
@@ -4,8 +4,17 @@
  *  @author Annabel Jump
  */
 
-public class SampleableListImpl implements SampleableList{
+public class SampleableListImpl extends ArrayList implements SampleableList{
+    /**
+     * Returns a list containing the first, third, fifth...
+     * items of this list, or an empty list if the list is empty.
+     *
+     * @return a list containing the first, third, fifth... items of this list
+     */
+    public SampleableList sample(){
+        
 
+    }
 
 
 }
