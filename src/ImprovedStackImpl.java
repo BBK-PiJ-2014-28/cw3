86b010686dc8bdb2a5f647c510f817d89607e896
Annabel Jump <annabel.jump@gmail.com>
2015-02-02 19:39:45 +0000

    More methods added

--- src/ImprovedStackImpl.java
+++ src/ImprovedStackImpl.java
@@ -20,5 +20,36 @@ public class ImprovedStackImpl implements ImprovedStack {
         myStack = new StackImpl(myList);
     }
 
-    
+    /**
+     * now all methods from StackImpl should be inserted
+     * to allow full functionality of ImprovedStackImpl
+     */
+    @Override //just in case
+    public boolean isEmpty(){
+        if(myStack.isEmpty()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int size(){
+        return myStack.size();
+    }
+
+    @Override
+    public void push(Object item){
+        myStack.push(item); //push() in StackImpl will add() item
+    }
+
+    @Override
+    public ReturnObject top(){
+        return myStack.top();
+    }
+
+    @Override
+    public ReturnObject pop(){
+        return myStack.pop();
+    }
 }
