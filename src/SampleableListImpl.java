930c634bd580334d7f9ddb8c51592a2d94b6487f
Annabel Jump <annabel.jump@gmail.com>
2015-02-02 15:41:56 +0000

    Added (empty) SampleableListImpl

---
+++ src/SampleableListImpl.java
@@ -0,0 +1,11 @@
+/**
+ *  A class implementing SampleableList interface
+ *  By Annabel Jump for Programming in Java Coursework 3
+ *  @author Annabel Jump
+ */
+
+public class SampleableListImpl implements SampleableList{
+
+
+
+}
