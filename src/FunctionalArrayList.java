0c0fe8b3b355e636fca5bf1d495bc5748a2e3cf3
Annabel Jump <annabel.jump@gmail.com>
2015-02-02 15:00:26 +0000

    Head() method completed, moving on

--- src/FunctionalArrayList.java
+++ src/FunctionalArrayList.java
@@ -6,7 +6,27 @@
  */
 
 public class FunctionalArrayList extends ArrayList implements FunctionalList {
+    /**
+     *{@InheritDoc}
+     */
+    public ReturnObject head(){
+        if(isEmpty()){
+            return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+        } else {
+            ReturnObject requestedObject = get(0);
+            return requestedObject;
+        }
+    }
 
+    /**
+     * Returns a list with the elements in this list except the
+     * head. The elements must be in the same order. The original list
+     * must not change or be affected by changes in the new list.
+     *
+     * If the list is empty, another empty list is returned.
+     */
+    public FunctionalList rest(){
 
-    
+    }
+
 }
