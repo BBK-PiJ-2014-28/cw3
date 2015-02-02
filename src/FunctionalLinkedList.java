4c392e959f816c0013ae3b9659e0c404df2f981b
Annabel Jump <annabel.jump@gmail.com>
2015-02-02 15:20:49 +0000

    First commit FunctionalLinkedList

---
+++ src/FunctionalLinkedList.java
@@ -0,0 +1,12 @@
+/**
+ *  A class implementing FunctionalList interface
+ *  Extends LinkedList
+ *  By Annabel Jump for Programming in Java Coursework 3
+ *  @author Annabel Jump
+ */
+
+public class FunctionalLinkedList extends LinkedList implements FunctionalList{
+
+    
+
+}
